17e14d4342d672675358ad2d77f66f6604d4bb61
Fraddosse_Laptop <fraddosse@hotmail.com>
2025-11-18 01:37:45 +0100

    Added page to see notes

--- ProjectTracker/Notes/views.py
+++ ProjectTracker/Notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Note, Tag
 
 # Create your views here.
+def index(request):
+	context = {"Notes" : Note.objects.all()}
+	return render(request, "Notes/Notes.html", context)
