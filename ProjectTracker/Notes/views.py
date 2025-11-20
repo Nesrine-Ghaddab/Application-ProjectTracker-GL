2d555b02a59cbc13e06441471fc254f22415220b
Fraddosse_Laptop <fraddosse@hotmail.com>
2025-11-20 03:40:46 +0100

    Edit tags linked to note functionnality added

--- ProjectTracker/Notes/views.py
+++ ProjectTracker/Notes/views.py
@@ -32,7 +32,7 @@ def edit_note(request, note_id):
 			return redirect('Notes:Details_Note', note_id=note.id)
 	else:
 		form = NoteForm(instance=note)
-	return render(request, "Notes/Edit_Note.html", {"note":form, "note_id": note_id, "date_update": note.update_at})
+	return render(request, "Notes/Edit_Note.html", {"note_object": note, "note":form, "note_id": note_id, "date_update": note.update_at})
 
 def delete_note(request, note_id):
 	note = Note.objects.get(pk = note_id)
