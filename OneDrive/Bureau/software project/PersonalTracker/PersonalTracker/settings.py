02159abf817fcfa7722df16edc95b9e3d6e6d2cb
Moataz003 <brahmi.motaz@gmail.com>
2025-12-09 19:31:59 +0100

    Clear email host user and password
    Removed sensitive email credentials from settings.

--- OneDrive/Bureau/software project/PersonalTracker/PersonalTracker/settings.py
+++ OneDrive/Bureau/software project/PersonalTracker/PersonalTracker/settings.py
@@ -144,8 +144,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "Brahmi.motaz@gmail.com"
-EMAIL_HOST_PASSWORD = "mrae aqdv emct xfmz"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
